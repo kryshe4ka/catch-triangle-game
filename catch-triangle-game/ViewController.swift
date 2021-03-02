996ea6a915fda113165dd9e2498c1732973a520c
Liza Kryshkovskaya <kryshe4ka@gmail.com>
2021-03-02 20:02:01 +0300

    create custom view element

--- catch-triangle-game/ViewController.swift
+++ catch-triangle-game/ViewController.swift
@@ -14,13 +14,11 @@ class ViewController: UIViewController {
     }
     
     @IBOutlet weak var timeLabel: UILabel!
-    @IBOutlet weak var gameFieldView: UIView!
+    @IBOutlet weak var gameFieldView: GameFieldView!
     @IBOutlet weak var stepper: UIStepper!
     @IBOutlet weak var actionButton: UIButton!
-    @IBOutlet weak var shapeX: NSLayoutConstraint!
-    @IBOutlet weak var shapeY: NSLayoutConstraint!
-    @IBOutlet weak var gameObject: UIImageView!
-    @IBAction func objectTapped(_ sender: UITapGestureRecognizer) {
+    
+    func objectTapped() {
         guard isGameActive else { return }
         repositionImageWithTimer()
         score += 1
@@ -76,7 +74,7 @@ class ViewController: UIViewController {
     }
     
     private func updateUI() {
-        gameObject.isHidden = !isGameActive
+        gameFieldView.isShapeHidden = !isGameActive
         stepper.isEnabled = !isGameActive
         if isGameActive {
             timeLabel.text = "Осталось \(Int(gameTimeLeft)) сек"
@@ -98,10 +96,7 @@ class ViewController: UIViewController {
     }
     
     @objc private func moveImage() {
-        let maxX = gameFieldView.bounds.maxX - gameObject.frame.width
-        let maxY = gameFieldView.bounds.maxY - gameObject.frame.height
-        shapeX.constant = CGFloat(arc4random_uniform(UInt32(maxX)))
-        shapeY.constant = CGFloat(arc4random_uniform(UInt32(maxY)))
+        gameFieldView.randomizeShapes()
     }
     
     
@@ -112,6 +107,9 @@ class ViewController: UIViewController {
         gameFieldView.layer.borderColor = UIColor.gray.cgColor
         gameFieldView.layer.cornerRadius = 5
         updateUI()
+        gameFieldView.shapeHitHandler = { [weak self] in
+            self?.objectTapped()
+        }
     }
 
 
